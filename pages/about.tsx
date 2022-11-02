f0fe8b83b99b93489ac0a3f68d1e67ba441609aa
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-02 16:26:12 +0600

    changes again

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import About from "../../components/About/About";
+import About from "../components/About/AboutPage";
 
 
 export default function AboutPage() {
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,4 +1,4 @@
-import Work from '../../components/Work/Work';
+import Work from '../components/Work/WorkPage';
 
 export default function WorkPage(){
      return <>
