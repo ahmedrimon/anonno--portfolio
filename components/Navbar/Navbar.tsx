18a9181359af0c74d25c2e90c1aa923258ffbf69
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-02 16:18:18 +0600

    first commit

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+// import { useLayoutEffect, useRef } from "react";
+// import gsap from "gsap";
+import Link from "next/link";
+
+export default function Navbar() {
+
+     // const navbar = useRef();
+
+     // useLayoutEffect(() => {
+
+     //      const ctx = gsap.context(() => {
+     //           gsap.to(".nav", { rotation: "+=360" });
+     //      }, navbar)
+
+     //      return () => ctx.revert()
+     // }, [])
+
+     return <section>
+          <nav className="absolute top-[10%] left-[95%] rotate-90 font-montreal__medium text-md font-normal cursor-pointer">
+               <Link href="/work">WORK</Link>
+               <Link href="/about" className="ml-4">ABOUT</Link>
+          </nav>
+     </section>
+}
+
+// absolute top-[10%] left-[93%] rotate-90
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'pt': ['PT Serif'],
+        'antiga': ['Antiga'],
+        'montreal': ['PP Neue Montreal Book'],
+        'montreal__medium': ['PP Neue Montreal Medium'],
+
+      },
+    },
+  },
+  plugins: [],
+}
