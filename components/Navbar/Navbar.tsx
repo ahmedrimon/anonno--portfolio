68517c49249080907aa977339a1aa4c40fe1d979
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-03 12:26:33 +0600

    nav responsive

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -16,11 +16,11 @@ export default function Navbar() {
      // }, [])
 
      return <section>
-          <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[420px]:top-[10%] max-[420px]:left-[77%]">
+          <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[1925px]:absolute max-[1925px]:max-w-[1%] max-[1925px]:top-[5%] max-[1925px]:left-[96%] , max-[767px]:absolute max-[767px]:max-w-[1%] max-[767px]:top-[5%] max-[767px]:left-[93%]">
                <Link href="/work">WORK</Link>
                <Link href="/about" className="ml-4">ABOUT</Link>
           </nav>
      </section>
 }
 
-// absolute top-[10%] left-[93%] rotate-90
+// absolute top-[10%] left-[93%] rotate-90 max-[420px]:top-[10%] max-[420px]:left-[77%]
