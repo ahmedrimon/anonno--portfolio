9e140e4f098dc82c0e4e943c678a78b052c1318d
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-03 14:48:31 +0600

    started

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-// import { useLayoutEffect, useRef } from "react";
-// import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 import Link from "next/link";
 
 export default function Navbar() {
 
-     // const navbar = useRef();
+     const link = useRef();
+  
+     useLayoutEffect(() => {
+          let ctx = gsap.context(() => {
+               gsap.to(".box", { rotation: "+=360" });
+          }, link);
+     
+          return () => ctx.revert();
+     }, []);
 
-     // useLayoutEffect(() => {
-
-     //      const ctx = gsap.context(() => {
-     //           gsap.to(".nav", { rotation: "+=360" });
-     //      }, navbar)
-
-     //      return () => ctx.revert()
-     // }, [])
 
      return <section>
           <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[1925px]:absolute max-[1925px]:max-w-[1%] max-[1925px]:top-[5%] max-[1925px]:left-[96%] , max-[767px]:absolute max-[767px]:max-w-[1%] max-[767px]:top-[5%] max-[767px]:left-[93%]">
-               <Link href="/work">WORK</Link>
+               <Link ref={link} className="box" href="/work">WORK</Link> 
                <Link href="/about" className="ml-4">ABOUT</Link>
           </nav>
      </section>
