227ae4e7b24e528f5aededdb5afc2293f692423d
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-04 12:50:16 +0600

    comma removed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ export default function Navbar() {
 
 
      return <section>
-          <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[1925px]:absolute max-[1925px]:max-w-[1%] max-[1925px]:top-[5%] max-[1925px]:left-[96%] , max-[767px]:absolute max-[767px]:max-w-[1%] max-[767px]:top-[5%] max-[767px]:left-[93%]">
-               <Link href="/work">WORK</Link> 
+          <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[1925px]:absolute max-[1925px]:max-w-[1%] max-[1925px]:top-[5%] max-[1925px]:left-[96%] max-[767px]:absolute max-[767px]:max-w-[1%] max-[767px]:top-[5%] max-[767px]:left-[93%]">
+               <Link href="/work">WORK</Link>
                <Link href="/about" className="ml-4">ABOUT</Link>
           </nav>
      </section>
