70e9c33f5e8a5f8cb4ebb591916109e03f20fb0d
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-06 10:47:24 +0600

    changes some space

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 export default function Navbar() {
 
-
      return <section>
           <nav className="absolute rotate-90 font-montreal__medium text-md font-normal cursor-pointer max-[1925px]:absolute max-[1925px]:max-w-[1%] max-[1925px]:top-[5%] max-[1925px]:left-[96%] max-[767px]:absolute max-[767px]:max-w-[1%] max-[767px]:top-[5%] max-[767px]:left-[93%]">
                <Link href="/work">WORK</Link>
