b4712d57ebc3f82204698ce066eba5461830d823
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-07 12:21:31 +0600

    new modified screen

---
+++ components/Screen/Screen.tsx
@@ -0,0 +1,5 @@
+export default function Screen() {
+     return <section>
+          <h1 className="font-antiga font-normal text-[14vw]">Work Inside</h1>
+     </section>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
+import Screen from "../components/Screen/Screen";
 
 export default function Home() {
 
      return <div className="bg-[#F7ECDD] h-screen w-screen overflow-x-hidden">
           <Navbar />
           <Hero />
+          <Screen />
      </div>
 }
