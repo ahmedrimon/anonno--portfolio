3be55fdddba3c4b4b24bae80d2182609e719b80f
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-07 14:47:49 +0600

    add something

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useRef } from "react"
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Screen() {
+     const el = useRef(null);
+
+     useEffect(() => {
+          gsap.to(".third", {
+               scrollTrigger: {
+                    trigger: ".third"
+               },
+               x: 400,
+               rotation: 360,
+               duration: 3
+          });
+     }, [])
+
      return <section>
           <h1 className="font-antiga font-normal text-[10vw] ml-[5em]">Work Inside</h1>
+          <main className="mb-[5%]">
+               <div className="flex justify-center items-center">
+                    <div className="first flex justify-center items-center font-graphik text-[2em] bg-indigo-400 bg-contain w-[5%]">a</div>
+               </div>
+               <div className="flex justify-center items-center">
+                    <div className="second flex justify-center items-center font-graphik text-[2em] bg-indigo-400 mt-3 w-[5%]">b</div>
+               </div>
+               <div className="flex justify-center items-center">
+                    <div ref={el} className="third flex justify-center items-center font-graphik text-[2em] bg-indigo-400 mt-3 w-[5%]">c</div>
+               </div>
+          </main>
      </section>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
 import Screen from "../components/Screen/Screen";
 
+
 export default function Home() {
+
 
      return <div className="bg-[#F7ECDD] h-screen w-screen overflow-x-hidden">
           <Navbar />
