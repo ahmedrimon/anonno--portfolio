d9742f682c55d1ad4972d5e2a5fd78eecc856787
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-10 10:24:23 +0600

    hlw

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -6,9 +6,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function Screen() {
 
+     // let sections = gsap.querySelectorAll(".panel");
 
-     return <section>
-          <h1 className="font-antiga font-normal text-[10vw] ml-[5em]">Work Inside</h1>
+     // useEffect(() => {
+     //      gsap.to(".panel", {
+     //           xPercent: -100 * ((".panel").length - 1),
+     //           ease: "none",
+     //           scrollTrigger: {
+     //                trigger: ".container",
+     //                pin: true,
+     //                scrub: 1,
+     //                snap: 1 / ((".panel").length - 1),
+     //                // base vertical scrolling on how wide the container is so it feels more natural.
+     //                start: "top center",
+     //                end: "+=4500",
+     //                markers: true
+     //           }
+     //      });
+     // }, [])
 
+     // return 
+     return <section className="h-[20%] p-[5%]">
+          <h1>Hlw</h1>
      </section>
 }
