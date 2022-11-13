e548d62d4bdb8bc8f7de5c63dd9e075c8742659f
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-13 18:51:02 +0600

    anser

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -9,38 +9,52 @@ export default function Screen() {
      // let el = useRef();
      // let q = gsap.utils.selector(el);
 
-     const components = document.querySelectorAll(".panel")
-     const container: HTMLElement | null = document.querySelector("#container")
+     let sections = gsap.utils.toArray(".panel");
+
+     // useEffect(() => {
+     //      gsap.to(components, {
+     //           xPercent: -100 * (components.length - 1),
+     //           ease: "none",
+     //           scrollTrigger: {
+     //                trigger: container,
+     //                pin: true,
+     //                scrub: 1,
+     //                // snap: 1 / (components.length - 1),
+     //                end: () => "+=" + container?.offsetWidth
+     //           }
+     //      });
+     // }, [])
 
      useEffect(() => {
-          gsap.to(components, {
-               xPercent: -100 * (components.length - 1),
+          gsap.to(sections, {
+               xPercent: -100 * (sections.length - 1),
                ease: "none",
                scrollTrigger: {
-                    trigger: container,
+                    trigger: "#container",
                     pin: true,
                     scrub: 1,
-                    // snap: 1 / (components.length - 1),
-                    end: () => "+=" + container?.offsetWidth
+                    snap: 1 / (sections.length - 1),
+                    // base vertical scrolling on how wide the container is so it feels more natural.
+                    end: "+=3500",
                }
           });
      }, [])
 
      // return 
      return <section id="container" className="flex flex-nowrap w-[600%] h-[100%]">
-          <section className="panel w-screen h-[100%] bg-red-500">
+          <section className="panel h-[100%] bg-red-500">
                ONE
           </section>
-          <section className="panel w-screen h-[100%] bg-orange-500">
+          <section className="panel bg-orange-500">
                TWO
           </section>
-          <section className="panel w-screen h-[100%] bg-purple-500">
+          <section className="panel bg-purple-500">
                THREE
           </section>
-          <section className="panel w-screen h-[100%] bg-green-500">
+          <section className="panel bg-green-500">
                FOUR
           </section>
-          <section className="panel w-screen h-[100%] bg-gray-500">
+          <section className="panel bg-gray-500">
                FIVE
           </section>
      </section>
