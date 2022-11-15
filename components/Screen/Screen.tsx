a345c45b9a4782a54dcc88c6a2c30be35baad5a9
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-15 11:19:49 +0600

    process start

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import prototype from '../../images/Ptototyping.webp';
 
 export default function Screen() {
@@ -7,7 +8,10 @@ export default function Screen() {
                <h1 className="font-cardinal font-normal text-[2vw]">PROTOTYPING</h1>
           </div>
           <div>
-               <img src={prototype} alt="" />
+               <h3 className="font-graphik font-bold text-[16px] leading-6 uppercase">First start Figma to sketch file and see what's the different file and structure uses. how it real life happens</h3>
+          </div>
+          <div>
+               <Image src={prototype} alt="prototyping image" width="400" height="400" />
           </div>
      </section>
 
