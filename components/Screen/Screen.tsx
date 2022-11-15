02d8c9d70df41798a8dacee91293ec0492b85a0d
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-15 12:15:36 +0600

    added image

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import prototype from '../../images/Ptototyping.webp';
+import prototype from '../../images/Prototype.webp';
 
 export default function Screen() {
 
@@ -7,12 +7,14 @@ export default function Screen() {
           <div className="relative rotate-90 w-[5%] top-[45%] border-r-2">
                <h1 className="font-cardinal font-normal text-[2vw]">PROTOTYPING</h1>
           </div>
-          <div>
-               <h3 className="font-graphik font-bold text-[16px] leading-6 uppercase">First start Figma to sketch file and see what's the different file and structure uses. how it real life happens</h3>
-          </div>
-          <div>
-               <Image src={prototype} alt="prototyping image" width="400" height="400" />
-          </div>
+          <section className="flex justify-center items-center">
+               <div className="flex justify-start mt-[32em] w-[20%]">
+                    <h3 className="font-graphik font-bold text-[16px] leading-6 uppercase">First start Figma to sketch file and see what's the different file and structure uses. how it real life happens</h3>
+               </div>
+               <div className="flex justify-end w-[50%]">
+                    <Image src={prototype} alt="prototyping image" width="400" height="400" />
+               </div>
+          </section>
      </section>
 
 }
