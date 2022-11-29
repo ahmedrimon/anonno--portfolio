61d69f3673e6b069fd2a5896f14a3d68db0b24d2
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-29 11:28:07 +0600

    oke

--- components/Asset/Asset.tsx
+++ components/Asset/Asset.tsx
@@ -1,5 +1,6 @@
 export default function Asset() {
      return <section className="h-[200vh] bg-[#DCFE4A]">
           <h1 className="font-antiga font-normal text-[12vw]">ASSET BUILDING</h1>
+          <p>WebP</p>
      </section>
 }
