784a2ef88f7f1d2582f45f701f3fa82bbf1a5070
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-11-30 12:50:14 +0600

    clear responsive asset file

--- components/Asset/Asset.tsx
+++ components/Asset/Asset.tsx
@@ -6,32 +6,32 @@ import three from "../../images/three.jpg";
 export default function Asset() {
      return <section className="h-[160vh] bg-[#DCFE4A]">
           <h1 className="font-antiga font-normal text-[12vw]">ASSET BUILDING</h1>
-          <main className="flex justify-start ml-4">
-               <div className="border-2 border-orange-500 rounded-full p-2 w-[10%] flex justify-center">
+          <main className="flex justify-start ml-4 max-[767px]:block">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">WebP</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Woff</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Png</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Vimeo</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Wordpress</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Figma</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Html</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Css</p>
                </div>
-               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center">
+               <div className="border-2 border-orange-500 rounded-full ml-4 p-2 w-[10%] flex justify-center max-[767px]:w-[70%] max-[767px]:mt-5 max-[767px]:mx-auto">
                     <p className="font-graphik font-bold text-[1rem]">Javascript</p>
                </div>
           </main>
@@ -47,7 +47,7 @@ export default function Asset() {
           <main className="flex justify-center items-center">
                <div className="font-antiga font-normal text-[8em]">THING</div>
           </main>
-          <div className="absolute top-[370%] ml-6 mt-6">
+          <div className="absolute top-[370%] ml-6 mt-6 max-[767px]:hidden">
                <Image
                     className="rounded-full"
                     src={first}
@@ -57,17 +57,7 @@ export default function Asset() {
                     priority
                />
           </div>
-          {/* <div className="absolute top-[380%] left-[43%] ml-6 mt-6 z-1">
-               <Image
-                    className="rounded-full"
-                    src={second}
-                    alt="red picture first"
-                    width={200}
-                    height={200}
-                    priority
-               />
-          </div> */}
-          <div className="absolute top-[390%] left-[87%] ml-6 mt-6">
+          <div className="absolute top-[390%] left-[87%] ml-6 mt-6 max-[767px]:hidden">
                <Image
                     className="rounded-full"
                     src={three}
