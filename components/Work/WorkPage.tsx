df7d9307714852d383406d112b984ea7e6aa0aca
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-12-28 12:23:44 +0600

    started along

--- components/Work/WorkPage.tsx
+++ components/Work/WorkPage.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import icam from "../../images/Icam-four.png";
 
 export default function Work() {
      return <section className="bg-[#f4efe9] h-[200vh]">
@@ -7,6 +9,15 @@ export default function Work() {
                     <Link className="uppercase font-cardinal" href="/"> Back Home</Link>
                </button>
           </div>
-          <h2 className="font-acma font-normal text-[29px] w-[30%]">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like</h2>
+          <h2 className="font-acma font-normal text-[8vw] w-[30%]">Experiment Project</h2>
+          <Image
+               className="rounded-full"
+               src={icam}
+               alt="red picture first"
+               width={500}
+               height={500}
+               priority
+          />
+          <p className="text-[10vw] font-cardinal">Here is this that you work on</p>
      </section>
 }
