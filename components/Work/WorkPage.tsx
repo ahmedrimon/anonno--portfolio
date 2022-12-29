97b6e03458990eaf7b01a9979237fb7a47494d59
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-12-29 13:49:23 +0600

    running application

--- components/Work/WorkPage.tsx
+++ components/Work/WorkPage.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import icam from "../../images/Icam-four.png";
+import explore from "../../images/React-Explore.png";
+import portfolio from "../../images/Portfolio.png";
 import FooterLast from "../FooterLast/FooterLast";
 
 export default function Work() {
-     return <section className="bg-[#f4efe9] h-[195vh]">
+     return <section className="bg-[#f4efe9] h-[170vh]">
           <div className="pt-3 flex justify-end">
                <button className="bg-[#e6b375] text-black w-[7%] h-[2%] rounded-md font-bold">
                     <Link className="uppercase font-cardinal" href="/"> Back Home</Link>
@@ -27,20 +29,20 @@ export default function Work() {
           <div className="flex mt-[3em]">
                <Image
                     className="rounded-full ml-[2em]"
-                    src={icam}
-                    alt="red picture first"
+                    src={explore}
+                    alt="explore the site"
                     width={500}
                     height={500}
                     priority
                />
-               <Link className="font-antiga text-[3vw] text-red-500 ml-[2em]" href="https://icam-distribution.netlify.app" target="__blank">LIVE LINK</Link>
-               <Link className="font-antiga text-[3vw] text-red-500 mt-[2em]" href="https://github.com/ahmedrimon/icam-distribution" target="__blank">RAW CODE</Link>
-               <p className="font-graphik font-normal text-[1vw]">To simple Ecommerce portray</p>
+               <Link className="font-antiga text-[3vw] text-red-500 ml-[2em]" href="https://react-explore.netlify.app/" target="__blank">LIVE LINK</Link>
+               <Link className="font-antiga text-[3vw] text-red-500 mt-[2em]" href="https://github.com/ahmedrimon/react__explore" target="__blank">RAW CODE</Link>
+               <p className="font-graphik font-normal text-[1vw]">Explore a little bit space between pages</p>
           </div>
           <div className="flex mt-[3em]">
                <Image
                     className="rounded-full ml-[2em]"
-                    src={icam}
+                    src={portfolio}
                     alt="red picture first"
                     width={500}
                     height={500}
@@ -48,9 +50,9 @@ export default function Work() {
                />
                <Link className="font-antiga text-[3vw] text-red-500 ml-[2em]" href="https://icam-distribution.netlify.app" target="__blank">LIVE LINK</Link>
                <Link className="font-antiga text-[3vw] text-red-500 mt-[2em]" href="https://github.com/ahmedrimon/icam-distribution" target="__blank">RAW CODE</Link>
-               <p className="font-graphik font-normal text-[1vw]">To simple Ecommerce portray</p>
+               <p className="font-graphik font-normal text-[1vw]">This portfolio is just maid me to think to the organize things</p>
           </div>
-          <div className="mt-[10.5em]">
+          <div className="mt-[6em]">
                <FooterLast />
           </div>
      </section>
