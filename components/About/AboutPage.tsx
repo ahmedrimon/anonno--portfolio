4b8dbf35911f32f7b0eff569f420fda4072ab85f
ahmed rimon <ahmedrimon377472@gmail.com>
2025-11-08 14:11:36 +0600

    Done Full Website

--- components/About/AboutPage.tsx
+++ components/About/AboutPage.tsx
@@ -1,20 +1,28 @@
 import Image from 'next/image'
 import own from "../../images/anonnoPicture.jpg";
+import Link from 'next/link';
 
 export default function About() {
-     return <section className="bg-[#D7D8DD] h-[100vh]">
-          <h1 className="font-antiga text-white text-[6vw] ml-[2em]">Get in Touch</h1>
-          <h1 className="font-graphik text-[3vw] ml-[6em] hover:bg-[#aebee8] hover:cursor-pointer hover:w-[35%] max-[520px]:text-[1.5rem] max-[520px]:ml-[6rem] max-[520px]:mt-[1rem]">anonnoruddho@gmail.com</h1>
-          <div className="flex justify-around">
-               <p className="font-antiga text-black text-[3vw] uppercase w-[20%] mt-[5rem] ml-[5rem] max-[520px]:hidden">I'm here for just work above and beyond ideas</p>
-               <Image
-                    className="ml-16 rounded-md max-[520px]:mt-[4rem]"
-                    src={own}
-                    alt="owner of this website"
-                    width={300}
-                    height={100}
-               />
-          </div>
-          <p className="font-acma text-black text-[1.5vw] w-[40%] mt-[8rem] ml-[32rem] max-[520px]:ml-[3rem] max-[520px]:text-[1.5rem]">I Work at least 2.5+ year almost, some work i done personally. I love making website which is more visually stunning</p>
-     </section>
+     return <div>
+               <div className="ml-4">
+                    <button className="w-[16%] rounded-md">
+                         <Link className="" href="/">&larr;</Link>
+                    </button>
+               </div>
+          <section className="h-[100vh] w-full md:h-[h-80vh]">
+               <h1 className="font-antiga text-[6vw] pt-[2em] ml-3 md:ml-5">Get in Touch</h1>
+               <h1 className="font-graphik hover:bg-[#aebee8] hover:cursor-pointer hover:w-[35%] max-[520px]:ml-[6rem] max-[520px]:mt-[1rem] md:ml-4">anonnoruddho@gmail.com</h1>
+               <div className="flex justify-around">
+                    <p className="font-antiga text-black text-[3vw] uppercase w-[20%] mt-[5rem] ml-[5rem] max-[520px]:hidden">I'm here for just work above and beyond ideas</p>
+                    <Image
+                         className="ml-16 rounded-md max-[520px]:mt-[4rem]"
+                         src={own}
+                         alt="owner of this website"
+                         width={200}
+                         height={200}
+                    />
+               </div>
+               <p className="font-acma text-black text-[1.5vw] w-[50%] mt-[8rem] max-[520px]:ml-[3rem] md:ml-6 text-lg">I Work at least 2.5+ year almost, some work i done personally. I love making website which is more visually stunning</p>
+          </section>
+     </div>
 }
--- components/Work/WorkPage.tsx
+++ components/Work/WorkPage.tsx
@@ -6,21 +6,21 @@ import portfolio from "../../images/Portfolio.png";
 import FooterLast from "../FooterLast/FooterLast";
 
 export default function Work() {
-     return <section className="bg-[#f4efe9] h-[170vh]">
-          <div className="pt-3 flex justify-end">
-               <button className="bg-[#e6b375] text-black w-[7%] h-[2%] rounded-md font-bold">
-                    <Link className="uppercase font-cardinal" href="/"> Back Home</Link>
+     return <section className="bg-[#f4efe9] h-[255vh] w-full sm:h-[271vh] md:h-[244vh] xl:h-[200vh]">
+          <div className="pt-3 ml-4">
+               <button className="w-[16%] rounded-md">
+                    <Link className="" href="/">&larr;</Link>
                </button>
           </div>
-          <h2 className="font-acma font-normal text-[8vw] w-[30%]">Experiment Project</h2>
+          <h2 className="font-acma font-normal text-[8vw] w-[30%] ml-6 mt-4">Experiment Project</h2>
           <p className="font-antiga text-[4em] ml-[0.4em]">01</p>
           <div className="min-[767px]:flex">
                <Image
                     className="rounded-full ml-[2em]"
                     src={icam}
                     alt="red picture first"
-                    width={400}
-                    height={400}
+                    width={200}
+                    height={200}
                     priority
                />
                <Link className="font-antiga text-[3em] text-red-500 ml-[1.5em] max-[767px]:text-[2em]" href="https://icam-distribution.netlify.app" target="__blank">LIVE LINK</Link>
@@ -35,8 +35,8 @@ export default function Work() {
                     className="rounded-full ml-[2em]"
                     src={explore}
                     alt="red picture first"
-                    width={400}
-                    height={400}
+                    width={200}
+                    height={200}
                     priority
                />
                <Link className="font-antiga text-[3em] text-red-500 ml-[1.5em] max-[767px]:text-[2em]" href="https://react-explore.netlify.app/" target="__blank">LIVE LINK</Link>
@@ -51,8 +51,8 @@ export default function Work() {
                     className="rounded-full ml-[2em]"
                     src={portfolio}
                     alt="red picture first"
-                    width={400}
-                    height={400}
+                    width={200}
+                    height={200}
                     priority
                />
                <Link className="font-antiga text-[3em] text-red-500 ml-[1.5em] max-[767px]:text-[2em]" href="https://iftekhar-portfolio.netlify.app/" target="__blank">LIVE LINK</Link>
@@ -61,7 +61,7 @@ export default function Work() {
                     <p className="font-graphik font-normal text-[1em] mt-[1em] w-[70%]">Progress that happen, just continue the thing and establish idea</p>
                </div>
           </div>
-          <div className="mt-[6em] max-[520px]:mt-[1.5em] min-[1280px]:mt-[1em]">
+          <div className="mt-[6em] md:m max-[520px]:mt-[1.5em]">
                <FooterLast />
           </div>
      </section>
